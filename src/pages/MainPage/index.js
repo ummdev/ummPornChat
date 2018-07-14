2785fdfe4e0ffb8904b462b0b6472e669a86424b
lagmanzaza <lagmanmc@gmail.com>
2018-07-14 23:30:46 +0700

    [Page , HTTP] render porn star then filter

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,50 +1,54 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import './index.css'
 import ChatBoxComponent from '../../components/chatBox';
+import SuggestionBoxComponent from '../../components/suggestionPorn';
 import axios from 'axios';
-export default class MainPage extends Component{
-  constructor(props){
+export default class MainPage extends Component {
+  constructor(props) {
     super(props)
     this.state = {
       text: '',
-      listMessages: ["1","2","3"],
-      listPornStars: []
+      listMessages: [],
+      listPornStars: [],
+      listSuggestionPorns: []
     }
   }
 
-  sendText(){
+  sendText() {
     axios.get(`https://us-central1-ummproject-b4a9c.cloudfunctions.net/getSuggestion`)
-    .then(({data})=>{
-      this.setState({
-        listPornStars: data
+      .then(({data}) => {
+        console.log(data)
+        this.setState({listPornStars: data})
+        console.log(this.state)
       })
+  }
+
+  suggestion() {
+    const regex = new RegExp(this.state.text, 'g')
+    const listSuggestionPorns = this.state.listPornStars.filter((value) => regex.test(value.name))
+    this.setState({
+      listSuggestionPorns: listSuggestionPorns
     })
   }
 
-  suggestion(){
-    console.log(this.state.listPornStars)
-  }
-
-  checkListPornStars(event){
-    if(event.key === 'Enter' && this.state.listPornStars.length === 0){
-      this.sendText()
-    }else if(event.key === 'Enter' && this.state.listPornStars.length > 0){
-      this.suggestion()
-    }
-  }
-  render(){
-    return(
+  render() {
+    return (
       <div className="container">
         <ChatBoxComponent listMessages={this.state.listMessages}/>
+        <SuggestionBoxComponent listSuggestion={this.state.listSuggestionPorns}/>
         <div className="messageInput">
           <input
             className="message"
-            onChange={(event) => this.setState({
-              text: event.target.value
-            })}
-            onKeyPress={(event) =>this.checkListPornStars.bind(this , event)}/>
+            onChange={(event) => this.setState({text: event.target.value})}
+            onKeyPress={(event) => {
+              if (event.key === 'Enter' && this.state.listPornStars.length === 0) {
+                this.sendText()
+                this.suggestion()
+              } else if (event.key === 'Enter' && this.state.listPornStars.length > 0) {
+                this.suggestion()
+              }
+              }}/>
         </div>
-      </div>
-    )
+    </div>)
   }
 }
