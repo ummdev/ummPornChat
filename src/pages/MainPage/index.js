1c7391e06d1b4cd60d235349259fbad284e924fb
lagmanzaza <lagmanmc@gmail.com>
2018-07-14 17:30:00 +0700

    [Page , HTTP] chat page

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,24 +1,49 @@
 import React,{Component} from 'react';
 import './index.css'
 import ChatBoxComponent from '../../components/chatBox';
+import axios from 'axios';
 export default class MainPage extends Component{
   constructor(props){
     super(props)
     this.state = {
       text: '',
-      listMessages: ["1","2","3"]
+      listMessages: ["1","2","3"],
+      listPornStars: []
+    }
+  }
+
+  sendText(){
+    axios.get(`https://us-central1-ummproject-b4a9c.cloudfunctions.net/getSuggestion`)
+    .then(({data})=>{
+      this.setState({
+        listPornStars: data
+      })
+    })
+  }
+
+  suggestion(){
+    console.log(this.state.listPornStars)
+  }
+
+  checkListPornStars(event){
+    if(event.key === 'Enter' && this.state.listPornStars.length === 0){
+      this.sendText()
+    }else if(event.key === 'Enter' && this.state.listPornStars.length > 0){
+      this.suggestion()
     }
   }
   render(){
     return(
       <div className="container">
-      <ChatBoxComponent listMessages={this.state.listMessages}/>
-      <input
-        className="messageInput"
-        onChange={(event)=> this.setState({
-          text: event.target.value
-        })}/>
-      {this.state.text}
+        <ChatBoxComponent listMessages={this.state.listMessages}/>
+        <div className="messageInput">
+          <input
+            className="message"
+            onChange={(event) => this.setState({
+              text: event.target.value
+            })}
+            onKeyPress={(event) =>this.checkListPornStars.bind(this , event)}/>
+        </div>
       </div>
     )
   }
